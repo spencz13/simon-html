c119240fa934943ab3645aec20423c3d411d8aee
spencz13 <spencz13@byu.edu>
2023-10-01 01:52:11 -0600

    git commit simon-html

--- deployFiles.sh
+++ deployFiles.sh
@@ -19,11 +19,11 @@ printf "\n----> Deploying files for $service to $hostname with $key\n"
 
 # Step 1
 printf "\n----> Clear out the previous distribution on the target.\n"
-ssh -i "$key" ubuntu@$hostname << ENDSSH
-rm -rf services/${service}/public
-mkdir -p services/${service}/public
+ssh -i "$spencerhanna.com.pem" ubuntu@$worktestapp.com << ENDSSH
+rm -rf services/${simon}/public
+mkdir -p services/${simon}/public
 ENDSSH
 
 # Step 2
 printf "\n----> Copy the distribution package to the target.\n"
-scp -r -i "$key" * ubuntu@$hostname:services/$service/public
+scp -r -i "$spencerhanna.com.pem" * ubuntu@$worktestapp.com:services/$simon/public
